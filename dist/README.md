785877b91db6f9f667927073a682980b75654f1e
mattkohl82 <mattkohl82@gmail.com>
2020-05-16 15:24:23 -0500

    got a readme generating

---
+++ dist/README.md
@@ -0,0 +1,15 @@
+# rub buddy
+          ## Description
+          ### vsvsvsdvsdvsdv
+          ## Install
+          ### vdvsdvsdvsdv
+          ## Useage
+          ### undefined
+          ## Lincenses
+          ### MIT
+          ## Contributions
+          ### sdvsdvsdv
+          ## Tests
+          ### sdvsdvsdv
+          ## Issues
+          ### Any issues please contact mattkohl82 for GitHub or mattkohl82@gmail.com for email
--- index.js
+++ index.js
@@ -70,32 +70,32 @@ const questions = () => {
     ]);
 } 
 
-// function to write README file
-function writeToFile(fileName, data) {
-    const generateMarkdownResults = generateMarkdown(data)
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+//     const generateMarkdownResults = generateMarkdown(data)
+// }
 
-const writeToFile = fileContent => {
+// const writeToFile = fileContent => {
 
-    const generateMarkdownResults = generateMarkdown(data) 
+//     const generateMarkdownResults = generateMarkdown(data) 
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile('./dist/README.md', data, err => {
-        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-        if (err) {
-          reject(err);
-          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-          return;
-        }
+//     return new Promise((resolve, reject) => {
+//       fs.writeFile('./dist/README.md', data, err => {
+//         // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+//         if (err) {
+//           reject(err);
+//           // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+//           return;
+//         }
   
-        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-        resolve({
-          ok: true,
-          message: 'File created!'
-        });
-      });
-    });
-  };
+//         // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+//         resolve({
+//           ok: true,
+//           message: 'File created!'
+//         });
+//       });
+//     });
+//   };
 
 
 
@@ -108,11 +108,11 @@ Create your professional README
 `);
 questions()
     .then(data => {
-        return writeToFile(data);
+        fs.writeFile('./dist/README.md', generateMarkdown(data), err => {
+            if (err) throw err;
+            console.log('README created! Check out README.md in dist directory to see it!')
+        });
     })
-    .catch(err => {
-        console.log(err);
-    });
 }
-  
-init();  
+// function call to initialize program
+init();
